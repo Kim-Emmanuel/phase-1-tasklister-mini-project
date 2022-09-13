3509342746b88b434463fc211d5a3b66addcca6d
kim <kimemmanuel004@gmail.com>
2022-09-13 12:57:27 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const form = document.querySelector ('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildToDo(e.target.creatForm.value)
+    form.reset()
+  })
+})
+
+function buildToDo(todo){
+  const p = document.createElement('p');
+  const btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'X'
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  console.log(p)
+  document.getElementById('list').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
